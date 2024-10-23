b0ecdb5c0cd4bd1c9fe26ce65ec2248397ebed57
Abimbola-max <Haryishar2@gmail.com>
2024-10-23 14:49:50 +0100

    chapter two

---
+++ chapterExercises/chapter2/exponentnumbers.py
@@ -0,0 +1,5 @@
+number_one = int(input("Enter first number: "))
+number_two = int(input("Enter second number: "))
+
+exponent_value = int(number_one ** number_two)
+print("The exponent value is: ", exponent_value)
---
+++ chapterExercises/chapter2/heartratecalculator.py
@@ -0,0 +1,13 @@
+age = int(input("Enter age: "))
+
+heart_rate = 220 - age
+min_heart_rate = 0.5 * heart_rate
+max_heart_rate = 0.85 * heart_rate
+
+print(f"The minimum heart rate is: {min_heart_rate:.2f}, maximum heart rate is: {max_heart_rate:.2f}")
+
+
+
+
+
+
