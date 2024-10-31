d07af35d80a792570f6624174655ad79e51d5250
Abimbola <Haryishar2@gmail.com>
2024-10-31 08:05:09 +0100

    tasks

--- assignment/randomnumbers.py
+++ assignment/randomnumbers.py
@@ -1,22 +1,20 @@
 import random
 
-print('enter of a guess from 0-50')
-
-guess = int(input('Enter a guess: '))
-
 randomNumber = random.randint(1, 50)
 
-while (guess != randomNumber):
- 
-	if (guess > randomNumber):
-		print('Too high, try again', ' here is the guessed number ', randomNumber)
+print('Guess a number between 1 and 50')
 
-	else:
-		print('Too low, try again', ' here is the guessed number ', randomNumber)
+guess = 0
 
-guess = (input('Enter another guess: '))
-	
-       if (guess == randomNumber):
-	    print('correct', 'here is the guessed number ', randomNumber)
-	
+while guess != randomNumber:
+    
+    guess = int(input('Enter your guess: '))
 
+    if (guess > randomNumber):
+        print('Too high, try again.')
+
+    elif (guess < randomNumber):
+        print('Too low, try again.')
+
+    else:
+        print('Correct! The number was: ', randomNumber)
