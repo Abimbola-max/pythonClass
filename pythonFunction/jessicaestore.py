f83583746d6b39e3e9f26b37846ef57aafdabf72
Abimbola <Haryishar2@gmail.com>
2024-11-28 06:14:14 +0100

    tasks

--- pythonFunction/jessicaestore.py
+++ pythonFunction/jessicaestore.py
@@ -1,26 +1,86 @@
-userInput =int(input('Welcome to Jessicas E-Store! /n1. View Products /n2. Add to Cart /n3. Remove from Cart /n4. View Cart /n5. Checkout /n6. Exit'))
-match(userInput):
-	case 1 :
-		view_product()
-	case 2 : 
-		add_to_cart()
-	case 3 :
-	case 4 :
-	case 5 :
-	case 6 :
-	case_ :
+total = []
+cart = []
+product = []
+mainuserInput = 0
+
+
+
+def add_to_cart():
+	product = input("Enter the product name or 6 to exit:")
+	if product.lower() == 'laptop':
+		cart.append("laptop - $1000")
+		total.append(1000)
+		print("you Added  Laptop - $1000 to cart")
+	elif product.lower() == 'phone' :
+		cart.append("phone - $500")
+		total.append(500)
+		print ("you Added  Phone - $500 to cart")
+	elif product.lower() == 'headphones' :
+		cart.append("headphones - $100")
+		total.append(100)
+		print ("you Added  Headphones - $100 to cart")
+	elif product == '6' :
+		main_menu()
+	else :
+		print("invalid input product missmatch !!!") 
+		add_to_cart()	
+
+def remove_from_cart():
+	product = input("Enter the product name or 6 to exit:")
+	if product.lower() == 'laptop':
+		cart.remove("laptop - $1000")
+		total.remove(1000)
+		print("you Added  Laptop - $1000 to cart")
+	elif product.lower() == 'phone':
+		cart.remove("phone - $500")
+		total.remove(500)
+		print ("you Added  Phone - $500 to cart")
+	elif product.lower() == 'headphones' :
+		cart.remove("headphones - $100")
+		total.remove(100)
+		print ("you Added  Headphones - $100 to cart")
+	elif product == '6' :
+		main_menu()
+	else :
+		print("invalid input product missmatch !!!") 
+		add_to_cart()	
+
+
+
+def view_cart():
+	return print(cart)
+		
+
+
+def check_out():
+	checkout = 0
+	for money in total:
+		checkout += money
+	print(f"checkout page \nYour list of purchase \n{view_cart()} \n total is $-{checkout}")
+		
+
+	
 
 def view_product():
-	sellectedProduct = int(input(1. Laptop - $1000 /n2. Phone - $500 /n3. Headphones - $100))
-	match selectedProduct:
-		case 1: add_to_cart([1000],"Laptop - $")
-		case 2: add_to_cart(["500"], "Phone - $"])
-		case 3: add_to_cart([100],["Headphones - $"])
+	return "1. Laptop - $1000 \n2. Phone - $500 \n3. Headphones - $100"
 
-def add_to_cart(cart:str,products:int):
-	prices = []
-	product =[]
-	prices.append(cart)
-	product.append(products)
-	return f"{products} has been added to your cart"
-def remove_cart():
+def main_menu():
+	userInput = int(input("Welcome to Jessica's E-Store! \n1. View Products \n2. Add to Cart \n3. Remove from Cart \n4. View Cart \n5. Checkout \n6. Exit\nEnter a enter option :"))
+	if userInput == 1:
+		print(view_product())
+	elif userInput == 2:
+		add_to_cart()
+	elif userInput == 3:
+		remove_from_cart()
+	elif userInput == 4:
+		view_cart()
+	elif userInput == 5:
+		check_out()
+
+	elif userInput == 6:
+		mainuserInput = 6
+		
+			
+while (mainuserInput != 6):
+		view_product()
+		main_menu()
