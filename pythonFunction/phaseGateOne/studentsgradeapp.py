b5456bc1db9e72007e61327c02712e636db634b5
Abimbola <Haryishar2@gmail.com>
2024-12-04 07:37:35 +0100

    task

--- pythonFunction/phaseGateOne/studentsgradeapp.py
+++ pythonFunction/phaseGateOne/studentsgradeapp.py
@@ -6,5 +6,18 @@ def for_all_input():
     for count in range(number_of_students):
         no_of_all_subjects = int(input(f"Enter number of subjects for student {count + 1}: "))
 
+        scores = []
+
+      
+        for count in range(no_of_all_subjects):
+            score = float(input(f"Enter scores for student {count + 1}: "))
+            scores.append(score)
+
+        student_scores.append(scores)
+        total = sum(scores)  
+        average = total / no_of_all_subjects
+
+        print(f"The total grade for student {count + 1} is: {total}")
+        print(f"The average grade for student {count + 1} is: {average}")
 
 for_all_input()
---
+++ pythonFunction/phaseGateOne/test_studentsgradeapp.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+import studentsgradeapp
+
+class TestStudentsGradeApp(TestCase):
+	def test_that_students_grade_app_exists(self):
+		studentsgradeapp.for_all_input()
