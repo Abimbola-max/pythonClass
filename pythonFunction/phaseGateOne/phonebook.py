81ec2f5ea796d6fd9cfc517e30e31f4dc4eda119
Abimbola <Haryishar2@gmail.com>
2024-12-06 02:49:39 +0100

    tasks

--- pythonFunction/phaseGateOne/phonebook.py
+++ pythonFunction/phaseGateOne/phonebook.py
@@ -1,35 +1,51 @@
-def get_contact():
-	
-	name = input("Enter the name of the person's contact you want to save: ")
-
-	phone_number = int(input("Enter the phone number you want to save: "))
-
-	return {'name': name, 'phone number': phone_number}
-
-get_contact()
-
+option = True
+ 
 def get_options():
 
-	print("1. Add contact")
-	print("2. Remove contact")
-	print("3. Find contact by phone number")
-	print("4. Find contact by first name")
-	print("5. Find contact by last name")
-	print("6. Edit contact")
-
+	while(option):
 	
-get_options()
+		print("1. Add contact")
+		print("2. Remove contact")
+		print("3. Find contact by phone number")
+		print("4. Find contact by first name")
+		print("5. Find contact by last name")
+		print("6. Edit contact")
+		print("7. Exit")
+		
+		try:
+			options = int(input("Enter an option: "))
+
+			if 1 <= options or options <= 7:
+				return options
+
+			else:
+				print("Invalid option. Please enter a number between 1 and 7.")
+		except ValueError:
+			print("Invalid input. Please enter a number.")
+
 
 contacts = []
 
 def add_contacts():
-	savingContacts = get_contact()
-	contacts.append(savingContacts)
-	print("You have successfully added", savingContacts['name'])
+
+	name = input("Enter the name of the person's contact you want to save: ")
+
+	phone_number = int(input("Enter the phone number you want to save: "))
+
+	email_address = input("Enter email address: ")
+
+	contact = {'name': name, 'phone number': phone_number, 'email address': email_address}
+
+	contacts.append(contact)
+
+	print("You have successfully added", contact['name'])
+
 
 def remove_contact():
+
 	name_to_remove = input("Enter the name of of the contact to remove: ")
 	contact_removed = False
+
 	for contact in len(contacts):
 		if contacts[contact]['name'] == name_to_remove:
 			del contacts[contact]
@@ -40,71 +56,75 @@ def remove_contact():
 	else:
 		print("We could not find contact")
 
-add_contacts()
 
 def find_contact_by_phone():
 
 	phone_no_to_find = input("Enter phone number to find: ")
+	contact_found = False
+
 	for contact in contacts:
-		if contacts['phone_number'] == phone_no_to_find:
+		if contact['phone_number'] == phone_no_to_find:
 			print("contact found ooo", contact)
-			return
+			contact_found = True
+			break
 	print("contact with phone number", phone_no_to_find, "not found")
 
 
 
 def find_contact_by_first_name():
-
 	first_name_to_find = input("Enter the first name to find: ")
+	contact_found = False
 
-	for first_name in contacts:
-		if first_name_to_find in contacts['name']:
-			print("contact found ooo", first_name)
-			return
-	print("contact with first name", first_name_to_find , "not found")
+	for contact in contacts:
+		if first_name_to_find in contact['name'].split():  
+			print("Contact found:", contact)
+			contact_found = True
+			break
 
-
-
+	if not contact_found:
+		print("Contact with first name", first_name_to_find, "not found.")
 
 def find_contact_by_last_name():
-
 	last_name_to_find = input("Enter the last name to find: ")
+	contact_found = False
 
-	for last_name in contacts:
-		if last_name_to_find in contacts['name']:
-			print("contact found ooo", last_name)
-			return
-	print("contact with last name", last_name_to_find, "not found")
+	for contact in contacts:
+		if last_name_to_find in contact['name'].split()[-1]:  
+			print("Contact found:", contact)
+			contact_found = True
+			break
 
+	if not contact_found:
+		print("Contact with last name", last_name_to_find, "not found.")
 
 def edit_contact():
+	name_to_edit = input("Enter the name of the contact to edit: ")
+	contact_found = False
 
-	
-	name_to_edit = input("Enter the last name to find: ")
-
-	for edit_name in contacts:
-		if contacts[edit_name]['name'] == name_to_edit:
-			print("Current contact details:", contacts[edit_name])
+	for count in range(len(contacts)):
+		if contacts[count]['name'] == name_to_edit:
+			print("Current contact details:", contacts[count])
 			new_name = input("Enter the new name (leave blank to keep current name): ")
 			new_phone_number = input("Enter the new phone number (leave blank to keep current number): ")
-			if new_name:
-				contacts[edit_name]['name'] = new_name
-			if new_phone_number:
-				contacts[edit_name]['phone_number'] = new_phone_number
+		if new_name:
+			contacts[count]['name'] = new_name
+		if new_phone_number:
+			contacts[count]['phone_number'] = new_phone_number
 			print("Contact updated successfully.")
-			return
-	print("Contact not found.")
+			contact_found = True
+			break
+
+		if not contact_found:
+			print("Contact not found.")
 
 program_running = True
-
 while program_running:
-				
 	options = get_options()
-
+    
 	match(options):
 		case 1:
-			add_contact()
-		case 2: 
+			add_contacts()
+		case 2:
 			remove_contact()
 		case 3:
 			find_contact_by_phone()
@@ -116,5 +136,6 @@ while program_running:
 			edit_contact()
 		case 7:
 			program_running = False
-		case _:
-			print("Invalid option.")
+		case _: 
+			print("Invalid option.") 
+
