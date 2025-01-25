707a27ea1c5d550bfd39de2354697bd062f44f08
Abimbola <Haryishar2@gmail.com>
2025-01-25 02:30:28 +0100

    tasks

---
+++ myarraymethods/myarraymethods.py
@@ -0,0 +1,45 @@
+class MyArrayMethod:
+
+    def __init__(self, size):
+        self.size = 0
+        self.capacity = size
+        self.array = [None] * size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def add_element(self, element):
+        if self.size < self.capacity:
+            self.array[self.size] = element
+            self.size += 1
+        else:
+            return "full"
+
+
+    def extend_element(self, elements):
+        for count in elements:
+            if self.size < self.capacity:
+                self.array[self.size] = count
+                self.size += 1
+            else:
+                return "full"
+
+    def insert_index(self, element, index):
+        for count in range(self.size, index, -1):
+            self.array[count] = self.array[count - 1]
+
+        self.array[index] = element
+        self.size += 1
+        return "Element inserted"
+
+    def get_index_of_an_element(self, element):
+        for index_counter in range(self.size):
+            if self.array[index_counter] == element:
+                return index_counter
+        return -1
+
+
+
+
+
+
