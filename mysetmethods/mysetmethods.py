46cd5d4ebf5bd5e1c3314c1a308777f9753a77f9
Abimbola <Haryishar2@gmail.com>
2025-01-26 07:02:35 +0100

    task

--- mysetmethods/mysetmethods.py
+++ mysetmethods/mysetmethods.py
@@ -1,4 +1,18 @@
+from myarraymethods import MyArrayMethod
+
+
 class MySet:
 
-    def __init__(self):
+    def __init__(self, size):
+        self.array_method = MyArrayMethod(size)
+
+    def is_empty(self):
+        return True
+
+    def add_element(self, element):
+        self.array_method.add_element(element)
+
+
+
+
 
--- mysetmethods/test_mysetmethods.py
+++ mysetmethods/test_mysetmethods.py
@@ -1,7 +1,15 @@
 import unittest
+
 from mysetmethods import MySet
 
 
 class MySetTestCase(unittest.TestCase):
     def test_that_my_set_methods_exist(self):
-        self.assertEqual(True, False)
+        array_method = MySet(4)
+        self.assertTrue(array_method.is_empty())
+
+
+    def test_that_my_set_methods_can_add_items(self):
+        array_method = MySet(6)
+        array_method.add_element("34")
+        self.assertFalse(array_method.is_empty())
