d250cb5d0a288293c4cd5c5bf05aa7fce6a35131
Abimbola <Haryishar2@gmail.com>
2025-01-28 02:49:48 +0100

    tasks

--- myarraymethods/datastructure/myqueuemethods.py
+++ myarraymethods/datastructure/myqueuemethods.py
@@ -1,10 +1,18 @@
 from datastructure.myarraymethods import MyArrayMethod
 
-
 class MyQueue:
 
-    def __init__(self):
-        self.queue = MyArrayMethod
+    def __init__(self, size):
+        self.queue = MyArrayMethod(size)
+
+    def is_empty(self):
+        return self.queue.is_empty()
+
+    def add(self, element):
+        self.queue.add_element(element)
+
+
+
 
 
 
--- mysetmethods/datastructure/test_myqueuemethods.py
+++ mysetmethods/datastructure/test_myqueuemethods.py
@@ -1,9 +1,22 @@
-import queue
 import unittest
 
+from datastructure.myqueuemethods import MyQueue
 
-class MyQueueMethodTest(unittest.TestCase):
-    def test_something(self):
-        self.assertTrue(queue.is_empty())
+
+class MyQueueMethodTestCase(unittest.TestCase):
+    def test_that_my_queue_is_empty(self):
+        my_queue = MyQueue(4)
+        self.assertTrue(my_queue.is_empty())
+
+    def test_that_my_queue_can_add_elements(self):
+        my_queue = MyQueue(4)
+        my_queue.add("50")
+        my_queue.add("60")
+        self.assertFalse(my_queue.is_empty())
+
+    def test_that_my_queue_can_do_peek_method(self):
+        my_queue = MyQueue(4)
+        
+
         
 
