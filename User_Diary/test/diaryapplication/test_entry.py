65f17edb73b298b20c155ee9f1102f3cdfef4435
Abimbola <Haryishar2@gmail.com>
2025-02-02 07:52:02 +0100

    tasks

---
+++ User_Diary/test/diaryapplication/test_entry.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyEntryTestCase(unittest.TestCase):
+    def test_that_entry_is_empty(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
