d2fc46095f99eab120db1432f30fc812e483003f
Abimbola <Haryishar2@gmail.com>
2025-02-05 13:05:09 +0100

    task

--- Televison Task/television.py
+++ Televison Task/television.py
@@ -1,10 +1,35 @@
 class Television:
 
     def __init__(self):
-        self.volume = 0
-        self.channel = 0
+        self.volume = 5
+        self.channel = 1
         self.is_on = False
-        self.television = None
+        self.mute = 0
 
     def is_tv_on(self):
         return self.is_on
+
+    def turn_off(self):
+        return self.is_on
+
+    def turn_on(self):
+        self.is_on = True
+        return self.is_on
+
+    def increase_volume(self):
+        if not self.is_on:
+            return self.volume
+        if self.volume > 50:
+            raise ValueError("Volume cannot exceed 50")
+        if self.volume == 0:
+            return self.mute
+        self.volume += 5
+        return self.volume
+
+    def decrease_volume(self):
+        if self.volume <= 0:
+            return self.mute
+        self.volume -= 5
+        return self.volume
+
+
--- Televison Task/test_television.py
+++ Televison Task/test_television.py
@@ -1,14 +1,46 @@
 import unittest
 
-import television
+from television import Television
 
 
 class MyTelevisionTestCase(unittest.TestCase):
     def test_that_television_is_off_by_default(self):
-        tv = television.Television()
+        tv = Television()
         self.assertEqual(False, tv.is_tv_on())
 
     def test_that_television_is_turned_on(self):
-        tv = television.Television()
-        self.assertEqual(True, tv.is_tv_on())
+        tv = Television()
+        tv.turn_off()
+        self.assertTrue(tv.turn_on())
+
+    def test_that_television_is_turned_off(self):
+        tv = Television()
+        tv.is_tv_on()
+        self.assertFalse(tv.turn_off())
+
+    def test_that_television_can_increase_volume_once_and_volume_returns_ten(self):
+        tv = Television()
+        tv.is_tv_on()
+        tv.turn_on()
+        tv.increase_volume()
+        self.assertEqual(10, tv.volume)
+
+    def test_that_television_can_increase_volume_twice_and_volume_return_fifteen(self):
+        tv = Television()
+        tv.is_tv_on()
+        tv.turn_on()
+        tv.increase_volume()
+        tv.increase_volume()
+        self.assertEqual(15, tv.volume)
+
+    def test_that_television_can_decrease_volume_one_volume_returns_zero_also_mute(self):
+        tv = Television()
+        tv.is_tv_on()
+        tv.turn_on()
+        tv.decrease_volume()
+        self.assertEqual(0, tv.volume)
+
+
+
+
 
